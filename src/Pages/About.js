65ceb96664bf6a1ec56a312de5600c64931e3d58
Harish-Shejokar <hshejokar@gmail.com>
2023-04-04 14:55:35 +0530

    understand NavLink

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 
 const About = () => {
   return (
     <div>
       <h2>About Page</h2>
-      <h3><Link to='/'>Back to Home page</Link></h3>
+      <h3><NavLink to='/'
+        className={(isActive) => {}}>Back to Home page</NavLink></h3>
     </div>
   )
 }
